aef3acabe679c3ea88ac793b4d748b15f4e205a3
arunkumar <karraarunkumar8888@gmail.com>
2023-06-06 15:27:25 +0530

    hii

---
+++ src/com/slokam/student.java
@@ -0,0 +1,18 @@
+package com.slokam;
+
+public class student {
+
+	int sid=1;
+	String sname="Arun";
+	Double smarks =78.90;
+	int sage=25;
+	
+	public static void main(String[] args) {
+
+		student s=new student();
+		
+		System.out.println(s.sid+","+s.sname+","+s.sage+","+s.smarks);
+		
+	}
+
+}
